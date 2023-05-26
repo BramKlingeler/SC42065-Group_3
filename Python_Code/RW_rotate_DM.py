7fffef2558d27c8ccce57d84b972082a75c520f4
Bram <51411007+BramKlingeler@users.noreply.github.com>
2023-05-26 11:52:24 +0200

    Add files via upload
    new revision

--- Python_Code/RW_rotate_DM.py
+++ Python_Code/RW_rotate_DM.py
@@ -1,6 +1,51 @@
+"""
+Example for PC in right corner
+
+Setup right corner:
+    Camera 1: Imaging camera
+    Camera 2: SH sensor
+"""
+from dm.okotech.dm import OkoDM
+
+from camera.ueye_camera import uEyeCamera
+from pyueye import ueye
+
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+import random
+
+SH_Sensor_Index = 2
+Camera_Index = 1
+
+
+def grabframes(nframes, cameraIndex=0):
+    with uEyeCamera(device_id=1) as cam:
+        cam.set_colormode(ueye.IS_CM_MONO8)  # IS_CM_MONO8)
+        w = 1280
+        h = 1024
+        # cam.set_aoi(0,0, w, h)
+
+        cam.alloc(buffer_count=10)
+        cam.set_exposure(18.5)
+        cam.capture_video(True)
+
+        imgs = np.zeros((nframes, h, w), dtype=np.uint8)
+        acquired = 0
+        # For some reason, the IDS cameras seem to be overexposed on the first frames (ignoring exposure time?).
+        # So best to discard some frames and then use the last one
+        while acquired < nframes:
+            frame = cam.grab_frame()
+            if frame is not None:
+                imgs[acquired] = frame
+                acquired += 1
+
+        cam.stop_video()
+
+    return imgs
+
 if __name__ == "__main__":
-    from dm.okotech.dm import OkoDM
-    import random
+    #from dm.okotech.dm import OkoDM
 
     print("started")
     # Use "with" blocks so the hardware doesn't get locked when you press ctrl-c
@@ -16,64 +61,70 @@ if __name__ == "__main__":
         # hint: Check what all actuators do indivudually -> not all mirrors are equal
         # hint: feel free to look up you model online for additional info.
 
-        #move the mirrors individually
+        #move the mirrors individually and ignoring
         num_actuators = len(dm)
         
 
         # Code for 1-D random walk
         
         # Probability to move up or down
-        prob = [0.15, 0.85] 
+        prob = [0.25, 0.75] 
  
         # statically defining the starting position
-        initial_voltage = random.randint(1,10) 
+        initial_voltage = random.randint(-10,10) 
         voltage = [initial_voltage]
  
         # creating the random points
         rr = np.random.random(25)
         downp = rr < prob[0]
         upp = rr > prob[1]
+        j = 0
  
         # for loop for making the walking process
         for idownp, iupp in zip(downp, upp):
-            down = idownp and voltage[-1] > 0
+            down = idownp and voltage[-1] > -10
             up = iupp and voltage[-1] < 10
             voltage.append(voltage[-1] - down + up)
             a = True
-            voltage_val = voltage*0.8/10.0
+            j = j+1
+            voltage_val = float(voltage[j])*0.8/10.0
+            
             while a == True:
-                s_time = 0.01  # sleep time (small amount of time between steps)
-                w_time = 0.5  # wait time around focus
+                s_time = 0.5  # sleep time (small amount of time between steps)
+                w_time = 2  # wait time around focus
                 steps = 10
+                pre_act_amp = 0
                 # increase actuator voltage gradually, then reverse, hold at 0
                 for i in range(steps):
                     current = np.ones(num_actuators)#np.zeros(num_actuators) for resetting the selected actuators
                     #current[j] = 1, only needed for seperate control
-                    act_amp = voltage_val / steps * current * (i + 1) #standard coeff                     dm.setActuators(act_amp)
-                    time.sleep(s_time)  # in seconds
-                    # print(act_amp[0])
-                for i in range(steps):
-                    act_amp = voltage_val / steps * current * (steps - i)
+                    act_amp = voltage_val / steps * current * (i + 1) + prev_act_amp / (i + 1) #standard coeff
                     dm.setActuators(act_amp)
                     time.sleep(s_time)  # in seconds
                     # print(act_amp[0])
+                #for i in range(steps):
+                    #act_amp = voltage_val / steps * current * (steps - i)
+                    #dm.setActuators(act_amp)
+                    #time.sleep(s_time)  # in seconds
+                    # print(act_amp[0])
 
-                dm.setActuators(np.zeros(len(dm)))
-                time.sleep(w_time)
+                #dm.setActuators(np.zeros(len(dm)))
+                #time.sleep(w_time)
 
                 # decrease actuator voltage gradually, then reverse, hold at 0
-                for i in range(steps):
-                    act_amp = -voltage_val / steps * current * (i + 1)
-                    dm.setActuators(act_amp)
-                    time.sleep(s_time)  # in seconds
+                #for i in range(steps):
+                    #act_amp = -voltage_val / steps * current * (i + 1)
+                    #dm.setActuators(act_amp)
+                    #time.sleep(s_time)  # in seconds
                     # print(act_amp[0])
-                for i in range(steps):
-                    act_amp = -voltage_val / steps * current * (steps - i)
-                    dm.setActuators(act_amp)
-                    time.sleep(s_time)  # in seconds
+                #for i in range(steps):
+                    #act_amp = -voltage_val / steps * current * (steps - i)
+                    #dm.setActuators(act_amp)
+                    #time.sleep(s_time)  # in seconds
                     # print(act_amp[0])
 
-                dm.setActuators(np.zeros(len(dm)))
+                #dm.setActuators(np.zeros(len(dm)))
+                prev_act_amp = act_amp
                 time.sleep(w_time)
                 a = False
 
@@ -92,6 +143,8 @@ if __name__ == "__main__":
                 img = grabframes(5, 2)
                 plt.imshow(img[-1])
                 plt.colorbar()
+                
+        dm.setActuators(np.zeros(len(dm)))
 
 
 print('finish operation')
