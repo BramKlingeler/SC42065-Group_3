51b857a5acf764c5528f8deb86c7754e0f139c99
HenanMechanists <133856599+HenanMechanists@users.noreply.github.com>
2023-06-26 22:01:31 +0200

    Add files via upload

--- Python_Code/New_try_626.py
+++ Python_Code/New_try_626.py
@@ -43,10 +43,21 @@ def grabframes(nframes, cameraIndex=0):
 
     return imgs
 
-def function(img):
+def max_intensity(img):
     intensity=np.amax(img[-1])
     return intensity
 
+def max_variance(img):
+    img_cal=img[-1]
+    var=np.var(img_cal)
+#    mean=np.mean(img_cal)
+#    N=np.size(img_cal)
+#    var=0
+#    img_cal.reshape((-1,1))
+#    for i in range(N):
+#        var=var+(img_cal[i]-mean)**2
+#    var=var/N
+    return var
 # loop of random walk
 if __name__ == "__main__":
     from dm.okotech.dm import OkoDM
@@ -64,38 +75,44 @@ if __name__ == "__main__":
             img_n=grabframes(5, Camera_Index)
             range_u=0.9
             u_0=act
-            while k<25:
+            while k<50:
                 img_o=img_n
                 u = u_0 + [np.random.uniform(-range_u,range_u) for i in range(variables)] # random vector
                 dm.setActuators(u)
                 time.sleep(0.1)
                 img_n=grabframes(5, Camera_Index)
                 plt.imshow(img_n[-1])
-                if(function(img_n)>function(img_o)): # if we find a better point
+                if(max_variance(img_n)>max_variance(img_o)): # if we find a better point
                     k = 1
                     img_n=img_n
                     plt.imshow(img_n[-1])
                     plt.figure()
                     u_op=u
-                    print(function(img_n))
+                    print(max_variance(img_n))
                     print(u)
                 else:
                     k += 1
                     img_n=img_o
-                    if k>20:
+                    if k>45:
                         range_u=0.5*range_u
                         u_0 = u_op
                         step=step+1
                         k=1
-                    if step>5:
+                    if step>8:
                         break
                 print(" %d time of random walk" % walk_num)
                 walk_num += 1
+#answer of intensity
 #[-0.78783364  0.46091063  0.60442698  0.81947666  0.34710144 -0.04037511
 #  0.33315949 -0.17724003  0.06887213  0.08255487  0.61794182  0.56773376
 # -0.94965443  0.12436213  0.83913617  0.42414116  0.88031037  0.71285349
 #  0.54134661]
-
+#answer of variance
+#[-0.62627906 -0.57748853 -1.02261049 -0.27942378  0.32247178  1.19441498
+# -0.5018988  -0.76241651  0.1200851  -0.53391441  0.5560053   1.21881588
+#  0.34127717 -0.24623419  0.82221155 -0.28707566 -0.96491517  0.16536202
+# -0.86804879]
+                
 #        u = [np.random.uniform(-1,1) for i in range(variables)] # random vector
 #    # u1 normalize of random vector
 #        u1 = [u[i]/math.sqrt(sum([u[i]**2 for i in range(variables)])) for i in range(variables)]
