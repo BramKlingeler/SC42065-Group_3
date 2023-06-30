a29636b11a49f5c42c341ce736e5214268b49344
HenanMechanists <133856599+HenanMechanists@users.noreply.github.com>
2023-06-30 14:40:25 +0200

    Add files via upload

--- Python_Code/classic_control_v4.py
+++ Python_Code/classic_control_v4.py
@@ -23,6 +23,157 @@ Camera_Index = 1
 # -0.86804879]
 act_initial= np.zeros([19])
 print(act_initial)
+#def create_reference_fixed_grid(img):
+#    img_gray=img
+#    #find threshod
+#    retv ,thresh=cv2.threshold(img_gray,50,255,cv2.THRESH_BINARY)
+#    #find contour
+#    thresh = thresh.astype(np.uint8)
+#    _, contours, _ = cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+#    #draw contour
+#    #cv2.drawContours(img_original,contours,-1,(0,0,255),3,lineType=cv2.LINE_AA)
+#    #show image
+#    #plt.imshow(draw)
+#    #cv2.waitKey()
+#    #cv2.destroyAllWindows()
+#    points = np.zeros((len(contours),2))
+#    i=0
+#    for contour in contours:
+#        # Calculate the moments of the contour
+#        moments = cv2.moments(contour)
+#        # Calculate the centroid coordinates
+#        if moments['m00'] !=0:
+#            center_x = int(moments['m10'] / moments['m00'])
+#            center_y = int(moments['m01'] / moments['m00'])
+#        else:
+#            center_x = 0
+#            center_y = 0
+#        # Add the point to the list
+#        points[i][0]=center_x
+#        points[i][1]=center_y
+#        i=i+1
+#    return points
+#
+#
+#def get_slopes(image, reference_centers):
+#    # Draw grid around reference centers
+#    image_d=image
+#    #image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+#    #grid_size = np.abs(reference_centers[1,0] - reference_centers[0,0])
+#    grid_size=58*0.9
+#    # Number of row and col of gird
+#    grid_rows = int((np.max(reference_centers[:, 1]) - np.min(reference_centers[:, 1])) / grid_size) + 1
+#    grid_cols = int((np.max(reference_centers[:, 0]) - np.min(reference_centers[:, 0])) / grid_size) + 1
+#    # draw the gird
+#    abs_centriod=np.zeros((len(reference_centers),2))
+#    for i in range(reference_centers.shape[0]):
+#        # Some definition
+#        width=int(grid_size/2)
+#        center_x = int(reference_centers[i,0])
+#        center_y = int(reference_centers[i,1])
+#        # Divide image into small grid matrix
+#        # Coordinate of opnecv and numpy.array is opposite!!
+#        small_grid = image[(center_y - width):(center_y + width), (center_x - width):(center_x + width)]
+#        # Find CoG of small matrix
+#        _, thresholded_image = cv2.threshold(small_grid, 50, 255, cv2.THRESH_BINARY)
+#        moments = cv2.moments(thresholded_image)
+#        r_centriod=np.zeros((2,1))
+#        if moments['m00'] !=0:
+#            r_centriod[0] = moments['m10'] / moments['m00']
+#            r_centriod[1] = moments['m01'] / moments['m00']
+#        else:
+#            r_centriod[0] = 0
+#            r_centriod[1] = 0
+#        abs_centriod[i,0] = center_x-width+r_centriod[0]
+#        abs_centriod[i,1] = center_y-width+r_centriod[1]
+#        # Draw the rectangle and position of CoG
+#    for i in range(reference_centers.shape[0]):
+#        center_x=int(reference_centers[i,0])
+#        center_y=int(reference_centers[i,1])
+#        
+#        draw_1=cv2.rectangle(image_d, (center_x-width,center_y-width),(center_x+width, center_y+width), (255,255,255), 2)
+#        draw_1=cv2.circle(draw_1, (int(abs_centriod[i,0]), int(abs_centriod[i,1])), 5, (255,255,255))
+#        draw_1=cv2.circle(draw_1, (center_x, center_y), 5, (0,255,255))
+#    new_draw = cv2.resize(draw_1, None, fx = 0.75, fy = 0.75)
+#    #cv2.imshow('grid',new_draw) 
+#    #cv2.waitKey()
+#    #cv2.destroyAllWindows()
+#    # Calculate deviation from reference center
+#    deviations = abs_centriod-reference_centers 
+#    return deviations
+#
+#def grabframes(nframes, cameraIndex=0):
+#    #with uEyeCamera(device_id=1) as cam:
+#    with uEyeCamera(device_id=2) as cam:
+#        cam.set_colormode(ueye.IS_CM_MONO8)  # IS_CM_MONO8)
+#        w = 1280
+#        h = 1024
+#        # cam.set_aoi(0,0, w, h)
+#
+#        cam.alloc(buffer_count=10)
+#        cam.set_exposure(0.908)
+#        cam.capture_video(True)
+#
+#        imgs = np.zeros((nframes, h, w), dtype=np.uint8)
+#        acquired = 0
+#        # For some reason, the IDS cameras seem to be overexposed on the first frames (ignoring exposure time?).
+#        # So best to discard some frames and then use the last one
+#        while acquired < nframes:
+#            frame = cam.grab_frame()
+#            if frame is not None:
+#                imgs[acquired] = frame
+#                acquired += 1
+#
+#        cam.stop_video()
+#
+#    return imgs
+#
+#def normalize_coordinates(points):
+#    # Calculate the center of the points
+#    center = np.mean(points, axis=0)
+#    # Calculate the maximum distance from the center to any point
+#    max_distance = np.max(np.linalg.norm(points - center, axis=1))
+#    # Normalize the coordinates
+#    normalized_points = (points - center) / max_distance
+#
+#    return normalized_points
+#
+#
+#def get_Bmatrix(points,Zorder):
+#    cart = RZern(Zorder)
+#    L, K = 2000, 2000
+#    ddx = np.linspace(-1.0, 1.0, K)
+#    ddy = np.linspace(-1.0, 1.0, L)
+#    xv, yv = np.meshgrid(ddx, ddy)
+#    cart.make_cart_grid(xv, yv)
+#
+#    cart.ZZ = np.zeros((xv.size, cart.nk),order='F', dtype=cart.numpy_dtype)
+#    cart.shape = xv.shape
+#    rho = np.sqrt(np.square(xv) + np.square(yv))
+#    theta = np.arctan2(yv, xv)
+#    for k in range(cart.nk):
+#        prod = cart.radial(k, rho) * cart.angular(k, theta)
+#        #Code below can be comment
+#        prod[rho > 1.0] = 0
+#        cart.ZZ[:, k] = cart.vect(prod)
+#
+#    B=np.zeros((2*points.shape[0],cart.nk))
+#    for i in range(cart.nk):
+#        #Define coefficient
+#        c = np.zeros((cart.nk,1))
+#        c[i] = 1
+#        Phi = cart.eval_grid(c, matrix=True)
+#        for j in range(points.shape[0]):
+#            #find where is point in pixel
+#            pixel_x=int(points[j,0]*L/2)
+#            pixel_y=int(points[j,1]*K/2)
+#            #Gradient of y in certain point
+#            B[2*j+1,i]=(Phi[pixel_y+1,pixel_x]-Phi[pixel_y-1,pixel_x])/2
+#            #Gradient of x in certain point
+#            B[2*j,i]=(Phi[pixel_y,pixel_x+1]-Phi[pixel_y,pixel_x-1])/2
+#    return B,cart
+
+
 def create_reference_fixed_grid(img):
     img_gray=img
     #find threshod
@@ -59,7 +210,7 @@ def get_slopes(image, reference_centers):
     # Draw grid around reference centers
     image_d=image
     #image=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
-    #grid_size = np.abs(reference_centers[1,0] - reference_centers[0,0])
+    #grid_size = np.abs(reference_centers[1,0] - reference_centers[0,0])*0.6
     grid_size=58*0.9
     # Number of row and col of gird
     grid_rows = int((np.max(reference_centers[:, 1]) - np.min(reference_centers[:, 1])) / grid_size) + 1
@@ -90,11 +241,12 @@ def get_slopes(image, reference_centers):
     for i in range(reference_centers.shape[0]):
         center_x=int(reference_centers[i,0])
         center_y=int(reference_centers[i,1])
-        
         draw_1=cv2.rectangle(image_d, (center_x-width,center_y-width),(center_x+width, center_y+width), (255,255,255), 2)
         draw_1=cv2.circle(draw_1, (int(abs_centriod[i,0]), int(abs_centriod[i,1])), 5, (255,255,255))
         draw_1=cv2.circle(draw_1, (center_x, center_y), 5, (0,255,255))
     new_draw = cv2.resize(draw_1, None, fx = 0.75, fy = 0.75)
+    #plt.figure()
+    #plt.imshow(new_draw)
     #cv2.imshow('grid',new_draw) 
     #cv2.waitKey()
     #cv2.destroyAllWindows()
@@ -154,7 +306,7 @@ def get_Bmatrix(points,Zorder):
     for k in range(cart.nk):
         prod = cart.radial(k, rho) * cart.angular(k, theta)
         #Code below can be comment
-        prod[rho > 1.0] = 0
+        #prod[rho > 1.0] = 0
         cart.ZZ[:, k] = cart.vect(prod)
 
     B=np.zeros((2*points.shape[0],cart.nk))
@@ -172,6 +324,7 @@ def get_Bmatrix(points,Zorder):
             #Gradient of x in certain point
             B[2*j,i]=(Phi[pixel_y,pixel_x+1]-Phi[pixel_y,pixel_x-1])/2
     return B,cart
+
 
 def rotate_image(image, angle):
     rows, cols = image.shape[:2]
@@ -196,7 +349,7 @@ def center_image(image):
 def image_fix(image):
     img_r = rotate_image(image, 4)
     img_c = center_image(img_r)
-    return img_c
+    return img_r
 
 if __name__ == "__main__":
     from dm.okotech.dm import OkoDM
@@ -227,7 +380,7 @@ if __name__ == "__main__":
         DM_ones = np.zeros((np.size(ref_points),num_actuators))
         DM_negative_ones = np.zeros((np.size(ref_points),num_actuators))
         print(np.shape(ref_points))
-        for j in range(num_actuators-2):
+        for j in range(num_actuators):
             # increase actuator voltage gradually, then reverse, hold at 0
             current = np.zeros(num_actuators)
             current[j] = 0.5
@@ -235,7 +388,7 @@ if __name__ == "__main__":
             dm.setActuators(act_amp)
             time.sleep(w_time)  # in seconds
             one_frame = grabframes(5,2)
-            one_frame = image_fix(one_frame[-1])
+            one_frame = image_fix(one_frame[-2])
             
             #plt.savefig('ref.jpg')
             #one_frame=cv2.imread('ref.jpg')
@@ -257,7 +410,7 @@ if __name__ == "__main__":
             dm.setActuators(act_amp)
             time.sleep(w_time)  # in seconds
             one_frame = grabframes(5,2)
-            one_frame = image_fix(one_frame[-1])
+            one_frame = image_fix(one_frame[-2])
                     
             slopes=np.reshape(get_slopes(one_frame,ref_points)-S_fix,(-1,1))
             for k in range(np.size(ref_points)):
@@ -296,7 +449,18 @@ if __name__ == "__main__":
         #Reference Coeffecients
         z=np.zeros((Z_order,1))
         z[1]=100
-
+        
+        
+        
+        
+        Phi0=cart.eval_grid(z, matrix=True)
+        fig = plt.figure()  
+        ax3 = plt.axes(projection='3d')
+        ax3.plot_surface(X,Y,Phi0,cmap='rainbow')
+        plt.title('Target')
+        #ax3.contour(X,Y,Z, zdim='z',offset=-2，cmap='rainbow)   
+        plt.show()
+        
         #Zernike to slope, S is desired slope
         S=np.dot(B,z)
 
@@ -309,14 +473,15 @@ if __name__ == "__main__":
         
         #act=np.zeros(len(dm))
         act=act_initial
-        act[0]=act[0]-0.6
+        act[0]=act[0]+0.6
 #        act[-2]=0.05
 #        act[-1]=-0.13
-
+        
         dm.setActuators(act)
-        
-        V=act
-        
+        V=np.zeros((19,1))
+        V[0]=-0.6
+        np.reshape(V, (19, 1))
+        print(np.shape(V))
         time.sleep(s_time)
         one_frame = grabframes(5,2)
         one_frame = image_fix(one_frame[-1])
@@ -327,7 +492,7 @@ if __name__ == "__main__":
         #test B matrix
         invB=np.linalg.pinv(B)
         test_cof=np.dot(invB,new_slopes)
-        print(test_cof)
+        print(np.shape(V))
         
         Phi=cart.eval_grid(test_cof, matrix=True)
 #        plt.figure()
@@ -342,10 +507,7 @@ if __name__ == "__main__":
         plt.title('Aberration')
         #ax3.contour(X,Y,Z, zdim='z',offset=-2，cmap='rainbow)   #等高线图，要设置offset，为Z的最小值
         plt.show()
-        
-        
-        
-        deltaS=S-new_slopes
+        deltaS = S-new_slopes
         square=0
         for j in range(np.size(deltaS)):
             square=square+deltaS[j]**2
@@ -355,18 +517,22 @@ if __name__ == "__main__":
         invC=np.linalg.pinv(C)
 
         #control slope
-        iterations=5
+        iterations=50
         for i in range (iterations):
             #RMS_old=RMS
 #            desired_V=np.dot(invC,S)
 #            V_mean = np.mean(desired_V)
 
             delta_V=np.dot(invC,deltaS)
-
-            V=V+delta_V
-            V_mean=np.zeros((19,1))
+            
+            V=V+0.2*delta_V
+            V_mean=np.mean(V)*np.ones((19,1))
+#            print(np.shape(V))
             #V_mean[0:17]=np.mean(V[0:17])
-            dm.setActuators(V-V_mean)
+            V=V-V_mean
+            
+            dm.setActuators(V)
+            #print(V)
             
 #            dm.setActuators(desired_V-V_mean)
             time.sleep(w_time)
@@ -375,10 +541,11 @@ if __name__ == "__main__":
             
             new_slopes=np.reshape(get_slopes(one_frame,ref_points)-S_fix,(-1,1))
             deltaS=S-new_slopes
+            
             square=0
             for k in range(np.size(deltaS)):
                 square=square+deltaS[k]**2
-            RMS=np.sqrt(square)/(np.size(deltaS))
+            RMS=np.sqrt(np.abs(square))/(np.size(deltaS))
             print(RMS)
 #            delta_RMS=RMS-RMS_old
 #            delta_RMS=delta_RMS/RMS
@@ -398,7 +565,7 @@ if __name__ == "__main__":
         dm.setActuators(act)
         
         
-        fig = plt.figure()  #定义新的三维坐标轴
+        fig = plt.figure()  
         ax3 = plt.axes(projection='3d')
         plt.title('Result')
 
@@ -431,8 +598,6 @@ if __name__ == "__main__":
 #
 ## Convert the matrix Z to a numpy array
 #Z = np.array(Z)
-
-
 
 
 
